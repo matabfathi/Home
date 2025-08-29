2be35cd76b16b23eaa11bf732aebd5ea759203b7
matabfathi <matabfathi127@gmail.com>
2025-08-30 02:46:13 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+ function openFunction(){
+    document.getElementById ('menu').style.width='200px';
+
+
+}
+function closeFunction(){
+    document.getElementById ('menu').style.width='0px';   
+
+}
