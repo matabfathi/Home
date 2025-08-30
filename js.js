206017474bc27ad51a43475da41d08d2d1e723f2
matabfathi <matabfathi127@gmail.com>
2025-08-30 19:19:41 +0300

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
- function openFunction(){
-    document.getElementById ('menu').style.width='200px';
-
-
-}
-function closeFunction(){
-    document.getElementById ('menu').style.width='0px';   
-
-}
